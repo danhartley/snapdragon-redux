f5e89ecd85f3f9d6b1e58d311c226f77f7a4a6ea
Daniel Hartley <danhartleybcn@gmail.com>
2018-04-08 11:08:43 +0100

    correct styling history page

--- src/ui/progress/history.js
+++ src/ui/progress/history.js
@@ -4,6 +4,8 @@ export const renderHistory = (history) => {
             
     const template = document.querySelector('.js-history-template');
 
+    DOM.leftBody.style.backgroundColor = 'rgb(50, 50, 50)';
+
     const historyText = template.content.querySelector('.js-history');
     
     let html = '';
