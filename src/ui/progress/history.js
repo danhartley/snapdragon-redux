7e605d592cca179d4db23082cc5b8255a53d69e0
Daniel Hartley <danhartleybcn@gmail.com>
2018-04-14 15:09:47 +0100

    sore + history updates for lessons

--- src/ui/progress/history.js
+++ src/ui/progress/history.js
@@ -1,24 +1,21 @@
 import { DOM } from 'ui/dom';
+import { store } from 'redux/store';
 
 export const renderHistory = (history) => {
             
+    const { items, score } = store.getState();
+
     const template = document.querySelector('.js-history-template');
 
     DOM.leftBody.style.backgroundColor = 'rgb(50, 50, 50)';
 
-    const historyText = template.content.querySelector('.js-history');
+    const txtRounds = template.content.querySelector('.js-history-rounds');
+    const txtAnswered = template.content.querySelector('.js-history-answered');
+    const txtScore = template.content.querySelector('.js-history-score');
     
-    let html = '';
-
-    history.forEach((score, index) => {
-         html+=
-            `<div>
-                <span>Round ${ index + 1}</span>
-                <p><span>total: ${score.total}</span> <span>correct: ${score.correct}</span></p>
-            </div>`;
-    });
-
-    historyText.innerHTML = html;
+    txtRounds.innerHTML = `You have completed ${items.currentRound} of ${items.rounds} rounds`;
+    txtAnswered.innerHTML = `You answered ${score.correct} out of ${score.total} correctly in the last round`;
+    txtScore.innerHTML = `Your score is ${history.correct} out of ${history.total} for this lesson`
 
     const clone = document.importNode(template.content, true);
     DOM.leftBody.innerHTML = '';
