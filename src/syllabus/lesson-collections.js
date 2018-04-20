a20bf0582b850e70f104c7c865554a6ab8a36d2b
Daniel Hartley <danhartleybcn@gmail.com>
2018-04-20 12:08:29 +0100

    new fauna da arrabida collection

--- src/syllabus/lesson-collections.js
+++ src/syllabus/lesson-collections.js
@@ -1,5 +1,6 @@
 import { flora, common, herbs } from 'api/flora';
 import { lamiaceae } from 'api/families';
+import { arrabida } from 'api/fauna.js';
 
 export const collections = [
     {
@@ -22,5 +23,12 @@ export const collections = [
         items: flora,
         eol_name: 'Flora Lisboa e Vale do Tejo',
         eol_link: 'http://eol.org/collections/124189'
+    },
+    {
+        id: '4',
+        name: 'fauna',
+        items: arrabida,
+        eol_name: 'Fauna da Arrábida',
+        eol_link: 'http://eol.org/collections/139346'
     }
 ];
