e4ded31e189583a79839424763334c229fba53ce
Daniel Hartley <danhartleybcn@gmail.com>
2018-04-25 16:44:26 +0100

    stamp template for collection

--- src/ui/screens/left/collections.js
+++ src/ui/screens/left/collections.js
@@ -12,27 +12,16 @@ export const renderCollections = () => {
 
     const template = document.querySelector('.js-collections-template');
 
-    const EOL = template.content.querySelector('.js-collections div:nth-child(1)');
-    const collectionRptr = template.content.querySelector('.js-collections div:nth-child(2)');    
-
-    EOL.innerHTML = `The following collections are hosted by the Encyclopedia of Life (EOL):`;
-
-    const collectionTable = collections.map(collection => {
-        return `<div class="collection">
-                    <button id="${collection.id}">Start learning</button> <a class="underline-link" target="_blank" href="${collection.eol_link}">EOL</a> <span>${collection.eol_name}</span> 
-                </div>`;
-    }).join(' ');
-
-    collectionRptr.innerHTML = collectionTable;
-
     const clone = document.importNode(template.content, true);
     
     DOM.leftBody.innerHTML = '';
-    DOM.leftBody.appendChild(clone);
-    
-    const currentCollection = document.querySelector('.js-collections .js-current-collection');
-    currentCollection.innerHTML = `The current collection is '${config.currentCollectionName}'`;
 
+    const data = { collections, config };
+
+    var ctx = new Stamp.Context();
+    var expanded = Stamp.expand(clone, data);
+    Stamp.appendChildren(DOM.leftBody, expanded);
+    
     const btns = document.querySelectorAll('.collection button');
 
     btns.forEach(btn => btn.addEventListener('click', event => {
