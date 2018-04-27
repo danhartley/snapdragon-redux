8b7c988284c969c4c5b6b39eca6188ef32b6850d
Daniel Hartley <danhartleybcn@gmail.com>
2018-04-27 10:24:39 +0100

    wordplay tests for cut up words

--- src/ui/helpers/wordplay.js
+++ src/ui/helpers/wordplay.js
@@ -1,24 +1,50 @@
 export const cutter = (str, length) => {
-    
+
+    str = str.replace(' ', '');
+
     const strs = [];
 
     for(let s of str) {
         strs.push(s);
     }
 
+    return recutter(strs, length);
+};
+
+const isMod = (length, strLength) => strLength % length === 0;
+
+const recutter = (strs, newLength) => {
+
+    let length = newLength || 4;
+    
     const cuts = [];
 
     let cut = '';
 
     strs.forEach( (str, index) => {
+        index++;
         cut += str;
-        if(index % length === 0) {
+        if(index !== 0 && index % length === 0) {
             cuts.push(cut);
             cut = '';
         }        
     });
 
-    console.log(cuts);
+    if(isMod(length, strs.length)) return cuts;
+
+    if(strs.length % length === 1) {
+        cuts[cuts.length-1] += strs.slice(-1);
+    } else if(strs.length % length === 2){
+        cuts[cuts.length-1] += strs.slice(-2).join('');
+    } else if(strs.length % length === 3){
+        cuts.push(strs.slice(-3).join(''));
+    }
+    else if(strs.length % length === 4){
+        cuts.push(strs.slice(-4).join('')); 
+    }
+    else {
+        recutter(strs, length)
+    }    
 
     return cuts;
-};
+}
--- src/ui/helpers/wordplay.test.js
+++ src/ui/helpers/wordplay.test.js
@@ -2,7 +2,36 @@ import { cutter } from 'ui/helpers/wordplay';
 
 test('cutter should return array of letters form input string', () => {
 
-    const species = 'Rosmarinus officinalis';
+    let species = 'Rosmarinus officinalis';
+    let cuts = cutter(species);
+    expect(cutter(species)).toEqual([ 'Rosm', 'arin', 'usof', 'fici', 'nalis' ]);
+    expect('Rosmarinusofficinalis').toEqual([ 'Rosm', 'arin', 'usof', 'fici', 'nalis' ].join(''));
+    expect(cutter(species,7)).toEqual([ 'Rosmari', 'nusoffi', 'cinalis']);
+    cuts = cutter(species, 7);
+    expect('Rosmarinusofficinalis').toEqual([ 'Rosmari', 'nusoffi', 'cinalis'].join(''));
 
-    expect(cutter(species, 3).length).toEqual(7);
+    species = 'Petroselinum crispum';
+    expect(cutter(species)).toEqual( ["Petr", "osel", "inum", "cris", "pum"]);
+
+    species = 'Artemisia dracunculus';
+    expect(cutter(species)).toEqual( ["Arte", "misi", "adra", "cunc", "ulus"]);
+
+    species = 'Thymus vulgaris';
+    expect(cutter(species)).toEqual( ["Thym", "usvu", "lgaris"]);
+
+    species = 'Foeniculum vulgare';
+    expect(cutter(species)).toEqual( ["Foen", "icul", "umvu", "lgare"]);
+
+    species = 'Foeniculum vulgare';
+    expect(cutter(species,5)).toEqual( ["Foeni", "culum", "vulgare"]);
+
+    species = 'Allium schoenoprasum';
+    expect(cutter(species,5)).toEqual( ["Alliu", "mscho", "enopr", "asum"]);
+
+    species = 'Allium schoenoprasum';
+    expect(cutter(species,6)).toEqual( ["Allium", "schoen", "oprasum"]);
+    
+    species = 'Allium schoenoprasum';
+    cuts = cutter(species,6);
+    expect('Alliumschoenoprasum').toEqual(cuts.join(''));
 });
