d2c5802a586639168fb52c13ea1f541349255eca
Daniel Hartley <danhartleybcn@gmail.com>
2018-05-21 17:43:27 +0100

    sw pre-caching

---
+++ workbox-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "globDirectory": "dist/",
+  "globPatterns": [
+    "**/*.{js,jpg,png,xml,ico,json,html}"
+  ],
+  "swDest": "dist/snapdragon-sw.js",
+  "swSrc": "dist/sw.js"
+};
