e28912b60a4802f7e8a3a28a9edd6f64f761a459
Daniel Hartley <danhartleybcn@gmail.com>
2018-06-16 16:10:31 +0100

    species list styling

--- src/ui/screens/common/species.js
+++ src/ui/screens/common/species.js
@@ -1,9 +1,12 @@
 import { DOM } from 'ui/dom';
+import { store } from 'redux/store';
 import { collections } from 'snapdragon/species-collections';
 import { renderTemplate } from 'ui/helpers/templating';
 import speciesTemplate from 'ui/screens/common/species-template.html';
 
 export const renderSpecies = (collectionId) => {
+
+    let { config} = store.getState();
 
     const template = document.createElement('template');
 
@@ -12,8 +15,21 @@ export const renderSpecies = (collectionId) => {
     DOM.leftBody.innerHTML = '';
 
     const collection = collections.filter(collection => collection.id === collectionId)[0];
-
-    const context = { collection };
+    collection.items.forEach(item => { 
+        item.image = item.images[0];
+        item.vernacularName = item.names.filter(name => name.language === config.language)[0].vernacularName;
+    });
 
     renderTemplate({ collection }, template.content, DOM.leftBody);
+
+    const listItemContainer = document.querySelector('.js-list-item-container');
+    const listItems = document.querySelectorAll('.js-list-item');
+
+    listItems.forEach(listItem => {        
+        listItem.addEventListener('click', (event) => {
+            listItems.forEach(item => item.classList.remove('active'));
+            const item = event.target;
+            item.classList.add('active');
+        })
+    });
 };
