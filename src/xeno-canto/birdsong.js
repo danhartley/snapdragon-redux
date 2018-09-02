36eb22830bed7fd96413e22a47f1184395780f23
Daniel Hartley <danhartleybcn@gmail.com>
2018-09-02 15:11:00 +0100

    Task 18919988: Birdsong

---
+++ src/xeno-canto/birdsong.js
@@ -0,0 +1,26 @@
+export const getBirdSong = (binomial, node, portrait) => {
+
+    const endpoint = `https://cors-anywhere.herokuapp.com/http://www.xeno-canto.org/api/2/recordings?query=${binomial} q:A`;
+
+    async function fetchAsync (endpoint) {
+
+        var request = new Request(endpoint);
+
+        var init = { mode: 'no-cors' };
+        
+        let response = await fetch(endpoint);
+        
+        let data = await response.json();
+        
+        return data;
+      }
+
+      fetchAsync(endpoint).then(data => {        
+        const recording = data.recordings[0];
+        const url = `https://www.xeno-canto.org/${recording.id}/embed?simple=1`;
+        portrait ? node.dataset.src = url : node.src = url;
+        if(portrait) node.style.display = 'initial';
+      });
+
+      return fetchAsync(endpoint, node, portrait);
+}
