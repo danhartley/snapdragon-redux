127faf8fac47fac0a81b32802635ba64d2009d2b
Daniel Hartley <danhartleybcn@gmail.com>
2018-09-17 17:51:12 +0100

    Task 19190346: mushroom trait info slider box

--- src/api/glossary/fungi.js
+++ src/api/glossary/fungi.js
@@ -5,18 +5,85 @@ export const fungi = [
         wiki: "https://en.wikipedia.org/wiki/Universal_veil"
     },
     {
-        term: "Saprotrophic nutrition",
-        definition: "Process of chemoheterotrophic extracellular digestion involved in processing of decayed organic matter.",
+        term: "Saprotroph",
+        definition: "Fungus that lives off decayed organic matter.",
         wiki: "https://en.wikipedia.org/wiki/Saprotrophic_nutrition"
     },
     {
-        term: "Gill (lamella)",
+        term: "Mycorrhizal",
+        definition: "Fungus that colonises roots of a vascular host plan in a (generally) mutualistic relationship.",
+        wiki: "https://en.wikipedia.org/wiki/Mycorrhiza"
+    },
+    {
+        term: "Lamella or Gill",
         definition: "Papery rib under the cap of some mushroom species (esp. agarics) used for spore dispersal.",
         wiki: "https://en.wikipedia.org/wiki/Lamella_(mycology)"
+    },
+    {
+        term: "Pore",
+        definition: "Mouth of a tube under the cap of some mushroom species (Bolete or Polypore) used for spore dispersal.",
+        wiki: "https://en.wikipedia.org/wiki/Bolete"
+    },
+    {
+        term: "Tooth or Spine",
+        definition: "Downward projections under the cap of some mushroom species (Tooth Fungi) used for spore dispersal.",
+        wiki: "https://en.wikipedia.org/wiki/Hydnoid_fungi"
     },
     {
         term: "Spore",
         definition: "Unit of sexual or asexual reproduction that may be adapted for dispersal and survival.",
         wiki: "https://en.wikipedia.org/wiki/Spore"
     },
+    {
+        term: "Volva",
+        definition: "Cup-like structure at the base of a mushroom.",
+        wiki: "https://en.wikipedia.org/wiki/Volva_(mycology)"
+    },
+    {
+        term: "Pileus",
+        definition: "Cap, or cap-like part, of a fungal fruiting body (basidiocarp) that supports spore-bearing surface (hymeniums).",
+        wiki: "https://en.wikipedia.org/wiki/Pileus_(mycology)"
+    },
+    {
+        term: "Hymenophore",
+        definition: "Hymenium-bearing structure of a fungal fruiting body (take form of smooth surfaces, lamellae, folds, tubes, or teeth).",
+        wiki: "https://en.wikipedia.org/wiki/Hymenophore"
+    },
+    {
+        term: "Mushroom",
+        definition: "Fleshy, spore-bearing fruiting body of a fungus, typically produced above ground on soil or on its food source.",
+        wiki: "https://en.wikipedia.org/wiki/Mushroom"
+    },
+    {
+        term: "Stipe",
+        definition: "Stem or stalk-like feature supporting the cap of a mushroom.",
+        wiki: "https://en.wikipedia.org/wiki/Stipe_(mycology)"
+    },
+    {
+        term: "Annulus",
+        definition: "Ring-like structure sometimes found on the stipe of some species of mushrooms.",
+        wiki: "https://en.wikipedia.org/wiki/Annulus_(mycology)"
+    },
+    {
+        term: "Skirt",
+        definition: "Ring flaring downwards and out."
+    },
+    {
+        term: "Gleba",
+        definition: "Fleshy spore-bearing inner mass of certain fungi such as the puffball or stinkhorn.",
+        wiki: 'https://en.wikipedia.org/wiki/Gleba'
+    },
+    {
+        term: "Adnate",
+        definition: "Gills are broadly attached to the stalk slightly above the bottom of the gill, with most of the gill fused to the stem."
+    },
+    {
+        term: "Decurrent",
+        definition: "Gills or pores are broadly attached to and extends down the stipe."
+    },
+    {
+        term: "Ostiole",
+        definition: "Small hole or opening through which algae[1] or fungi release their mature spores.",
+        wiki: "https://en.wikipedia.org/wiki/Ostiole"
+    }
 ]
