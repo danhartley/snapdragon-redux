fcc6706ee35774dfe6419f39670769f7a9fd5697
Daniel Hartley <danhartleybcn@gmail.com>
2018-10-30 14:09:53 +0000

    Task 20145702: limit feature length in portrait

--- src/ui/screens/common/feature.js
+++ src/ui/screens/common/feature.js
@@ -1,12 +1,19 @@
+import * as R from 'ramda';
+
 import { itemProperties } from 'ui/helpers/data-checking';
 import { renderTemplate } from 'ui/helpers/templating';
 import featureTemplate from 'ui/screens/common/feature-template.html';
 
 export const featureType = (collection, item, traits, config, parent, featureType) => {
 
-    const feature = itemProperties.itemContextProperty(traits, item, featureType);
+    let feature = itemProperties.itemContextProperty(traits, item, featureType);
 
     if(feature && feature !== '' && feature.length !== 0 && feature[0] !== '') {
+
+        if(config.isPortraitMode) {
+            feature = R.take(3, feature);
+        }
+
         const template = document.createElement('template');
         template.innerHTML = featureTemplate;
         let label;
