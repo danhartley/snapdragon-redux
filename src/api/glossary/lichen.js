2f9a2fd6b3e7ea2723de16d7d79b7457e26a14cd
Daniel Hartley <danhartleybcn@gmail.com>
2018-11-08 20:22:06 +0000

    Lichen

---
+++ src/api/glossary/lichen.js
@@ -0,0 +1,48 @@
+export const lichen = [
+    {
+        term: "Lichen",
+        definition: "Composite organism of algae or cyanobacteria and fungi living in a mutualistic relationship.",
+        wiki: "https://en.wikipedia.org/wiki/Lichen",
+        level: 2
+    },
+    {
+        term: "Fruticose",
+        definition: "Growing like tuft or multiple-branched leafless mini-shrub, upright or hanging down, flat or round 3D branches.",
+        wiki: "https://en.wikipedia.org/wiki/Fruticose_lichen",
+        level: 2
+    },
+    {
+        term: "Foliose",
+        definition: "Having flat, 2D leaf-like structures.",
+        wiki: "https://en.wikipedia.org/wiki/Foliose_lichen",
+        level: 2
+    },
+    {
+        term: "Crustose",
+        definition: "Forming a crust that strongly adheres to a substrate.",
+        wiki: "https://en.wikipedia.org/wiki/Crustose_lichen",
+        level: 2
+    },
+    {
+        term: "Leprose",
+        definition: "Having a powdery or granular surface.",
+        wiki: "https://en.wikipedia.org/wiki/Leprose_lichen",
+        level: 2
+    },
+    {
+        term: "Epiphyte",
+        definition: "An organism that grows on the surface of a plant and derives its moisture and nutrients from the air, rain, water or surrounding debris.",
+        wiki: "https://en.wikipedia.org/wiki/Epiphyte",
+        level: 2
+    },
+    {
+        term: "Thallus",
+        definition: "Nonreproductive tissues, or vegetative body part of a lichen. Usually visually prominent.", 
+        level: 2
+    },
+    {
+        term: "Thallus type",
+        definition: "Means of distinuguishing lichen e.g. foliose, crustose.", 
+        level: 2
+    },
+];
