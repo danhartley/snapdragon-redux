85cf7709716a577694a60719abdf6cbf0e06623c
Daniel Hartley <danhartleybcn@gmail.com>
2018-11-30 16:59:16 +0000

    Task: local species for Washington

--- src/api/inat/inat.js
+++ src/api/inat/inat.js
@@ -9,7 +9,7 @@ import { deciduousAndEvergreenTrees } from 'api/rhs/deciduous-evergreen-trees';
 
 import { birds } from 'api/snapdragon/common-city-birds';
 import { mushrooms } from 'api/snapdragon/mushrooms';
-import { weeds1 } from 'api/rhs/weeds1';
+import { weeds } from 'api/rhs/weeds';
 import { lichen } from 'api/snapdragon/lichen';
 
 import { plants } from 'api/snapdragon/plants';
@@ -24,7 +24,7 @@ export const getInatSpecies = (latitude, longitude) => {
         ...insects,
         ...herbsAndSpices,
         ...deciduousAndEvergreenTrees,
-        ...weeds1,
+        ...weeds,
         ...lichen,
         ...plants
     ];
