35e659da4d7a32b5e2a5422df98cd2a0dfe29956
Daniel Hartley <danhartleybcn@gmail.com>
2019-12-19 18:08:42 +0000

    Task: Portrait end of lesson partial fix

--- src/ui/screens/progress/summary.js
+++ src/ui/screens/progress/summary.js
@@ -19,54 +19,60 @@ export const renderSummary = history => {
 
     if(!lesson.isLessonComplete) return;
 
-    console.log('\x1b[32m', 'renderSummary');
+    const renderScreen = () => {
+        
+        console.log('\x1b[32m', 'renderSummary');
 
-    const template = document.createElement('template');
-
-    template.innerHTML = summaryTemplate;
-            
-    const parent = DOM.rightBody;
-    parent.innerHTML = '';
-
-    let header, summary, warning = ''; 
-
-    renderTemplate({ score, history, collection, config, header, summary, warning }, template.content, parent);
+        const template = document.createElement('template');
+              template.innerHTML = summaryTemplate;
+                
+        const parent = DOM.rightBody;
+              parent.innerHTML = '';
     
-    let actionLink = document.querySelector('.js-continue-link');
-
-    if(lesson.isLessonComplete) actionLink.innerHTML = 'Choose new';
-
-    const handleBtnClickEvent = event => {
-
-        subscription.remove(subscription.getByName('renderSummary'));
-        subscription.remove(subscription.getByName('renderHistory'));
-
-        if(lesson.isLessonComplete) {
-            lessonHandler.purgeLesson();
-        }
-        else lessonHandler.changeState(enums.lessonState.NEXT_ROUND, collection, config, history);
+        let header, summary, warning = ''; 
+    
+        renderTemplate({ score, history, collection, config, header, summary, warning }, template.content, parent);
+        
+        let actionLink = document.querySelector('.js-continue-link');
+    
+        if(lesson.isLessonComplete) actionLink.innerHTML = 'Choose new';
+    
+        const handleBtnClickEvent = event => {
+    
+            subscription.remove(subscription.getByName('renderSummary'));
+            subscription.remove(subscription.getByName('renderHistory'));
+    
+            if(lesson.isLessonComplete) {
+                lessonHandler.purgeLesson();
+            }
+            else lessonHandler.changeState(enums.lessonState.NEXT_ROUND, collection, config, history);
+        };
+    
+        actionLink.removeEventListener('click', handleBtnClickEvent);
+        actionLink.addEventListener('click', handleBtnClickEvent);
+    
+        const progress = document.querySelector('.js-progress');
+    
+        if(!progress) return;
+    
+        const scoreAverage = (score.correct/score.total);
+        const historyAverage = (history.correct/history.total);
+    
+        progress.innerHTML = scoreAverage === historyAverage
+                    ? 'Average' 
+                    : scoreAverage > historyAverage
+                        ? 'Above average'
+                        : 'Below average';
+    
+        scoreAverage === historyAverage
+                ? ''
+                : scoreAverage > historyAverage
+                    ? progress.classList.add('above')
+                    : progress.classList.add('below')
     };
 
-    actionLink.removeEventListener('click', handleBtnClickEvent);
-    actionLink.addEventListener('click', handleBtnClickEvent);
-
-    const progress = document.querySelector('.js-progress');
-
-    if(!progress) return;
-
-    const scoreAverage = (score.correct/score.total);
-    const historyAverage = (history.correct/history.total);
-
-    progress.innerHTML = scoreAverage === historyAverage
-                ? 'Average' 
-                : scoreAverage > historyAverage
-                    ? 'Above average'
-                    : 'Below average';
-
-    scoreAverage === historyAverage
-            ? ''
-            : scoreAverage > historyAverage
-                ? progress.classList.add('above')
-                : progress.classList.add('below')
+    setTimeout(() => {
+        renderScreen();
+    });
 };
 
