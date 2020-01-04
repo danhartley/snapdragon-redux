7db1ef7de202804ff6dc25849bac283292c3a8b3
Daniel Hartley <danhartleybcn@gmail.com>
2020-01-04 16:30:24 +0000

    Task: tidy before change

--- src/ui/screens/lists/lesson-list-header.js
+++ src/ui/screens/lists/lesson-list-header.js
@@ -14,39 +14,18 @@ export const renderLessonListHeader = parent => {
     const toggleCtrl = document.querySelector('.js-toggle-control');
           toggleCtrl.addEventListener('click', e => {
 
-            // toggleVideoFilterInputState();
+    e.stopPropagation();
 
-            e.stopPropagation();
-
-            const state = e.currentTarget.dataset.toggleState;
-            switch(state) {
-              case 'on': 
-                toggleCtrl.dataset.toggleState = 'off';
-                toggleElem.dataset.toggleState = 'off';
-                break;
-              case 'off':
-                toggleCtrl.dataset.toggleState = 'on';
-                toggleElem.dataset.toggleState = 'on';
-                break;
-            }
-          });
-
-    // const videoFilter = document.querySelector('.js-filter-by-video');
-    //       videoFilter.addEventListener('click', e => {
-    //         const lessonsWithoutVideo = document.querySelectorAll('div[data-has-video="false"]');
-    //         const checked = e.currentTarget.children[0].checked;
-    //         checked 
-    //           ? lessonsWithoutVideo.forEach(lesson => lesson.classList.remove('hide-important'))
-    //           : lessonsWithoutVideo.forEach(lesson => lesson.classList.add('hide-important'));
-    //       });
-    //       videoFilter.click();
+    const state = e.currentTarget.dataset.toggleState;
+    switch(state) {
+      case 'on': 
+        toggleCtrl.dataset.toggleState = 'off';
+        toggleElem.dataset.toggleState = 'off';
+        break;
+      case 'off':
+        toggleCtrl.dataset.toggleState = 'on';
+        toggleElem.dataset.toggleState = 'on';
+        break;
+    }
+  });
 };
-
-// const toggleVideoFilterInputState = () => {
-//     const input = document.querySelector('#chkVideo');
-//     if(input.hasAttribute('disabled')) {
-//       input.removeAttribute('disabled');
-//     } else {
-//       input.setAttribute('disabled', 'disabled');
-//     }
-// };
