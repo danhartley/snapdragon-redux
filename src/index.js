20d11b7a8fe53e9b644257f00c3c658b57df3a66
Daniel Hartley <danhartleybcn@gmail.com>
2020-07-03 17:39:29 +0100

    Task: video positioning

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import "babel-polyfill";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// import 'bootstrap/js/dist/carousel';
-// import 'bootstrap/js/dist/modal';
-// import 'bootstrap/js/dist/dropdown';
-
 import 'ui/css/snapdragon-colours.css';
 import 'ui/css/snapdragon.css';
 import 'ui/css/common.css';
