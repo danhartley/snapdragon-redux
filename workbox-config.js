25fe363b0282d6a3bfa576417ccf9d2b90e1762d
Daniel Hartley <danhartleybcn@gmail.com>
2020-07-20 17:22:07 +0100

    Task: pre add express

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "globDirectory": "dist/",
   "globPatterns": [
-    "**/*.{js,html,css,scss,woff,woff2,json}"
+    "**/*.{js,html,css,scss,woff,woff2,json,jpg,gif}"
   ],
   "swDest": "src/static/root/sw.js"
 };
