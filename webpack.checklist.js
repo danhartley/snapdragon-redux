ef2fb5a748877bd985bea9c2287e96930748b8de
Daniel Hartley <danhartleybcn@gmail.com>
2020-08-23 10:41:31 +0100

    Task: checklist prod

--- webpack.checklist.js
+++ webpack.checklist.js
@@ -4,7 +4,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 const WebpackLighthousePlugin = require('webpack-lighthouse-plugin'); 
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     devtool: "source-map",
     entry: {
       checklist: './src/checklist/checklist.js'
